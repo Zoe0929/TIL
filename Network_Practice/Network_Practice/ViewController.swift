210aead08bd4775b77094576a199ed24a98c8621
Zoe <mam07065@naver.com>
2024-06-11 10:14:15 +0900

    [Set/#1] 프로젝트 세팅

---
+++ Network_Practice/Network_Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Network_Practice
+//
+//  Created by 이지희 on 6/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
