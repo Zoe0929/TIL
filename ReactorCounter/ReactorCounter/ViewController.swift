685cdc7fa39ac7fa657726e64c4a15040406767e
Zoe <mam07065@naver.com>
2024-06-13 01:33:51 +0900

    [Set/#3] setting project

---
+++ ReactorCounter/ReactorCounter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  ReactorCounter
+//
+//  Created by 이지희 on 6/13/24.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    private func setUI() {
+        setHierarchy()
+        setConstraints()
+    }
+    
+    private func setHierarchy() {
+        
+    }
+    
+    private func setConstraints() {
+        
+    }
+
+}
+
